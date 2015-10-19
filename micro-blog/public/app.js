11a99b385be4a5943761467d8dd6d7a237478bfc
bgabot89 <bgabot89@gmail.com>
2015-10-19 09:42:18 -0700

    fixed server.js

--- micro-blog/public/app.js
+++ micro-blog/public/app.js
@@ -1,25 +1,24 @@
 // CLIENT-SIDE JAVASCRIPT
 
 console.log("JS is working"); 
-// On page load
+// Calls function, on submit
 $(document).ready(function(){
-  pageLoad();
+  Submitter();
 });
 
 // function definitions
 
-function pageLoad() {
+function Submitter() {
   // set event listeners
   $("#new-list-form").on("submit", function(e){
     // prevent form submission
     e.preventDefault();
     // post serialized form to server
     $.post("/api/list", $(this).serialize(), function(response){
-      // append new food to the page
+      // append lsit to the ejs
       var newList = response;
-      // clear new food form
-      var foodString = makeHTMLString(newlist);
-      $("#list-ul").prepend(foodString);
+      var listString = makeString(newlist);
+      $("#list-ul").prepend(listString);
       // reset the form 
       $("#submitButton")[0].reset();
     });
@@ -28,7 +27,8 @@ function pageLoad() {
 }
 
 
-function makeHTMLString(list){
+
+function makeString(list){
   return '<li class="list-group-item"><h4 class="list-group-item-heading">' + list.name +
   '</h4><button data-id='+ list.id + ' type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
   '</li>';
